0b5d82d99e283cd56e652be829c7b8dfed3de244
FernandoDzay <luisdzay.juegos@gmail.com>
2020-08-22 18:19:12 -0500

    Updates

---
+++ precache-manifest.3cae80b3c41e2c6e7c6e948720967379.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76752a101d267aef7dfe12b07d418e20",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "5684aed6f5b767d6ecbb",
+    "url": "/smart-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1725b1e5b304b57d4f0f",
+    "url": "/smart-brain/static/css/main.1b580841.chunk.css"
+  },
+  {
+    "revision": "5684aed6f5b767d6ecbb",
+    "url": "/smart-brain/static/js/2.32da4d6d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smart-brain/static/js/2.32da4d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1725b1e5b304b57d4f0f",
+    "url": "/smart-brain/static/js/main.77e0d32c.chunk.js"
+  },
+  {
+    "revision": "257db6b14da30691b4a8",
+    "url": "/smart-brain/static/js/runtime-main.1bac4abf.js"
+  },
+  {
+    "revision": "4100098c8af8b04589771aa58935f4c8",
+    "url": "/smart-brain/static/media/Brain.4100098c.png"
+  }
+]);
